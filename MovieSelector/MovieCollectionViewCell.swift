ef1b0246f122ff859e429254582ee8407f34e56d
Mukund Vemuri <tallDarkAndHandsome@mukunds-mbp.lan>
2017-08-31 01:35:01 -0400

    Updates to user interface for using CollectionViews. Also, added controller classes for these new views.

---
+++ MovieSelector/MovieCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionViewCell.swift
+//  MovieSelector
+//
+//  Created by Mukund Vemuri on 2017-08-30.
+//  Copyright © 2017 Mukund Vemuri. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+}
